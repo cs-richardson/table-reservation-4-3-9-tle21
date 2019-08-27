12d068cd63952a7a50b2fcffd2ed946b56795d89
Tu Nam Le <tle21@ssis.edu.vn>
2019-08-27 15:40:30 +0700

    Create tablereservation.py

---
+++ tablereservation.py
@@ -0,0 +1,9 @@
+reservation_name = "Rudolf"
+
+name = input("What is your name?: ") #prompting user for Name
+
+if name == reservation_name: #checking if name matches the name in reservation_name
+    print ("Right this way!") #If it does print "Right this way"
+
+else:
+    print ("Sorry, we don't have a reservation under that name") #If it does not then print "Sorry, we dont' have a reservation under that name
